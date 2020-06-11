fa70cf139fecc08a85447f23231a27e4bd86714f
Gustavo Carmo <gustavo.silva.carmo@hotmail.com>
2020-06-11 19:39:57 -0300

    Challenge Six gofinance

---
+++ src/repositories/TransactionsRepository.ts
@@ -0,0 +1,44 @@
+import { EntityRepository, Repository } from 'typeorm';
+
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+@EntityRepository(Transaction)
+class TransactionsRepository extends Repository<Transaction> {
+  public async getBalance(): Promise<Balance> {
+    const allTransactions = await this.find();
+
+    const balance = allTransactions.reduce(
+      (acumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            acumulator.income += Number(transaction.value);
+            break;
+          case 'outcome':
+            acumulator.outcome += Number(transaction.value);
+            break;
+          default:
+            break;
+        }
+
+        return acumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
+  }
+}
+
+export default TransactionsRepository;
